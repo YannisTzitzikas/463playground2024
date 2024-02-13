7a6344cb3fb837a4ba8991530d969560fe1b1110
YannisTzitzikas <tzitzik@ics.forth.gr>
2024-02-13 22:20:34 +0200

    first

---
+++ 463playground2024/src/Todelete.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+/**
+ * @author Yannis Tzitzikas (yannistzitzik@gmail.com)
+ *
+ */
+public class Todelete {
+
+	/**
+	 * 
+	 */
+	public Todelete() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("tsa");
+
+	}
+
+}

dc77d01763d71b4899ef8e7d45a804fb5a314fd9
Yannis Marketakis <yannismarketakis@gmail.com>
2025-03-16 17:45:07 +0200

    added client

--- 463playground2024/src/ZA_ExternalSystems/ElasticSearchClient.java
+++ 463playground2024/src/ZA_ExternalSystems/ElasticSearchClient.java
@@ -3,18 +3,43 @@
  */
 package ZA_ExternalSystems;
 
+import java.io.IOException;
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+
 /**
  * @author Yannis Tzitzikas (yannistzitzik@gmail.com)
- *
  */
 public class ElasticSearchClient {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+        RestHighLevelClient client = new RestHighLevelClient(
+            RestClient.builder(new HttpHost("localhost", 9200, "http"))
+        );
+        
+        SearchRequest searchRequest = new SearchRequest("multiple");
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+        sourceBuilder.query(QueryBuilders.matchQuery("age", "30"));
+        searchRequest.source(sourceBuilder);
 
-	}
+        // execute the search request
+        SearchResponse searchResponse = client.search(searchRequest, RequestOptions.DEFAULT);
+
+        // print search results
+        System.out.println("Total hits: " + searchResponse.getHits().getTotalHits().value);
+        for (SearchHit hit : searchResponse.getHits().getHits()) {
+            System.out.println("Found document: " + hit.getSourceAsString());
+        }
+
+        // Close the client
+        client.close();
+    }
 
 }

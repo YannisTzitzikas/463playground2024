9b8f1f4cded12f2b5d24a984a864998114bf1c4c
YannisTzitzikas <tzitzik@ics.forth.gr>
2025-03-16 15:03:45 +0200

    package for code that uses external Information Retrieval Systems

---
+++ 463playground2024/src/ZA_ExternalSystems/ElasticSearchClient.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package ZA_ExternalSystems;
+
+/**
+ * @author Yannis Tzitzikas (yannistzitzik@gmail.com)
+ *
+ */
+public class ElasticSearchClient {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
